9c48eb3ddaa44a126d5c6756e03671e42d934475
Pablo Manuel Fernández Velazquez <163730673+ProfitCoder@users.noreply.github.com>
2026-03-05 10:15:08 +0100

    Add files via upload

---
+++ Unidad 7/Ejemplo herencias/Alumno.java
@@ -0,0 +1,54 @@
+//@ProfitCoder 		9:24   05/03/2026
+
+import java.util.GregorianCalendar;
+
+public class Alumno extends Persona{
+	//Atributos
+	private String grupo;
+	private double notaMedia;
+	
+	//Constructores
+	public Alumno (String nombre, String apellidos, GregorianCalendar fechaNac, String grupo, double notaMedia){
+		super(nombre,apellidos,fechaNac);		//Sirve para llamar al constructor de la clase padre
+		this.grupo = grupo;
+		this.notaMedia = notaMedia;
+	}
+	
+	public Alumno(){}
+	
+	//Otros Metodos
+	public void setGrupo(String grupo){
+		this.grupo = grupo;
+	}
+	
+	public void setNotaMedia(double notaMedia){
+		this.notaMedia = notaMedia;
+	}
+	
+	public String getGrupo(){
+		return grupo;
+	}
+	
+	public double getNotaMedia(){
+		return notaMedia;
+	}
+	
+	
+	//Usamos Override porque hemos sobreescrito el metodo de la clase padre
+	@Override
+	public String getNombre(){
+		return "Alumno: " + nombre;
+	}
+	
+	//Metodo mostrar
+	@Override
+	public void mostrar(){
+		super.mostrar();
+		System.out.println("Grupo: " + grupo);
+		System.out.println("Nota Media: " + notaMedia);
+	}
+	
+	
+	
+	
+}
